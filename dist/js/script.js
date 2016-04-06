b5379e1768105025a202eaae4e45c06238368fc9
Jobayer Arman <carbonjha@gmail.com>
2016-04-06 19:15:51 +0600

    update 0.3.0
    + favicon added
    + more section in index.html
    + practice page
    + practice area in footer
    + style update
    + active link js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-	// write scripts here
+$(function activeLink() {
+  $('ul.nav a').filter(function(){
+    return this.href==location.href;}).parents("li").addClass('active').siblings().removeClass('active');
+  $('ul.nav a').click(function(){
+    $(this).parent().parents("li").addClass('active').siblings().removeClass('active');
+  });
 });
